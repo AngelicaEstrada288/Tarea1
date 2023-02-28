a11ded0f3b605be832b6922c296d7db125288bdd
DELL <DELL@ANGELICA>
2023-02-28 10:02:22 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1/Tarea1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Tarea1
+{
+    internal class Class1
+    {
+        public string nombre { get; set;}
+
+        public string apellidos { get; set;}
+
+        public string edad { get; set;}
+
+        public string correo { get; set;}
+
+
+    }
+}
---
+++ Tarea1/Tarea1/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviarinformacion_Clicked(object sender, EventArgs e)
+        {
+
+
+            var Rsul = new Class1
+            { 
+                nombre = txtnombre.Text,
+                apellidos = txtapellidos.Text,
+                edad = txtedad.Text,
+                correo = txtcorreo.Text,
+
+            };
+
+            var scondpnge = new Page2Resultado();
+            scondpnge.BindingContext= Rsul;
+
+            Navigation.PushAsync(scondpnge);
+
+
+        }
+               
+          
+
+     
+           
+
+
+
+
+        
+    }
+
+}
